9234bb57ad1aea8048aed3e779677960a2525ca8
Rodicio Julián <jrodicio98@gmail.com>
2020-06-16 22:49:17 -0300

    Se sube TP4 realizado hasta la funcion ll_get()

--- TP3/eclipse_tp3_v2/eclipse_tp3_v2/tp3_linux/Controller.h
+++ TP3/eclipse_tp3_v2/eclipse_tp3_v2/tp3_linux/Controller.h
@@ -1,7 +1,25 @@
 #include "parser.h"
 
+/* \fn int controller_loadFromText(char* path , LinkedList* pArrayListEmployee)
+ * \brief Permite cargar un archivo CSV de empleados al linkedlist.
+ * \param char* path: Ruta del archivo de empleados en formato CSV.
+ * \param LinkedList* pArrayListEmployee: LinkedList donde se insertará cada empleado.
+ * */
 int controller_loadFromText(char* path , LinkedList* pArrayListEmployee);
+
+/* \fn int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee)
+ * \brief Permite cargar un archivo BIN de empleados al linkedlist.
+ * \param char* path: Ruta del archivo de empleados en formato BIN.
+ * \param LinkedList* pArrayListEmployee: LinkedList donde se insertará cada empleado.
+ * */
 int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee);
+
+/* \fn int controller_loadFile(char* path, LinkedList* pArrayListEmployee, char* typeRead)
+ * \brief Permite cargar un archivo de empleados en un LinkedList, se especifica el tipo de lectura variable entre "r" y "rb".
+ * \param char* path: Ruta del archivo de empleados.
+ * \param char* typeRead: Tipo de lectura
+ * \param LinkedList* pArrayListEmployee: LinkedList donde se insertará cada empleado.
+ * */
 int controller_loadFile(char* path, LinkedList* pArrayListEmployee, char* typeRead);
 int controller_addEmployee(LinkedList* pArrayListEmployee);
 int controller_removeEmployee(LinkedList* pArrayListEmployee);
